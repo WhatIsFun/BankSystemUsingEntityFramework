e1404f7fcbeac0ab41ee439fca31cde28a019c98
WhatIsFun <mohammed110051@gmail.com>
2023-09-19 19:10:13 +0400

    BankSystemUsingEntityFramework

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankSystem_Using_Entity_Framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
